df6b141fe66a6813e57027c43a065de05818ea3f
vishal <vishalgithub123@gmail.com>
2025-11-09 20:18:38 +0530

    changing process.env to import.meta.env

--- frontend/Notes/src/api/axiosConfig.jsx
+++ frontend/Notes/src/api/axiosConfig.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // <-- set in .env or Vercel
+  baseURL: import.meta.env.REACT_APP_API_URL, // <-- set in .env or Vercel
   withCredentials: true, // set true if using cookies/sessions
 });
 
