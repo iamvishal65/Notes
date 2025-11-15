0c6a1d18319c3571b797ec1ed1e49504cb1ff71c
vishal <vishalgithub123@gmail.com>
2025-11-15 06:09:08 +0530

    fix axiosInstance by addibg meta

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL, // your frontend origin
+  origin: process.env.CLIENT_URL, 
   credentials: true
 }));
 app.use('/api/auth',authRoutes)
--- frontend/Notes/src/api/axiosConfig.jsx
+++ frontend/Notes/src/api/axiosConfig.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, 
 });
 
